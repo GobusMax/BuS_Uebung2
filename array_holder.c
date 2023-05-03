5a99d274a60fc42e220a6a63e644ba6a97aa10aa
Gobus <florian-c-hoffmann@t-online.de>
2023-05-03 20:28:36 +0200

    Copilot Magic!

--- array_holder.c
+++ array_holder.c
@@ -6,31 +6,125 @@
 #include "constants.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 int main(void) {
     int shared_memory_file_descriptor = shm_open(MEM_NAME, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
-    ftruncate(shared_memory_file_descriptor, sizeof(NUMBERS));
+    if (ftruncate(shared_memory_file_descriptor, sizeof(NUMBERS)) == -1) {
+        switch (errno) {
+            case 0:
+                break;
+            case EBADF:
+                printf("fd is not a valid open file descriptor");
+                return 1;
+            case EINVAL:
+                printf("The length argument was negative or larger than the maximum file size");
+                return 1;
+            case EFBIG:
+                printf("The length argument was larger than the current file size");
+                return 1;
+            default:
+                printf("Error truncating file");
+                return 1;
+        }
+    }
+
     int(*shared_memory_value)[N_ELEMS] = mmap(NULL, sizeof(NUMBERS), PROT_READ | PROT_WRITE, MAP_SHARED, shared_memory_file_descriptor, 0);
-    // shared_memory_value = NUMBERS;
+    if (shared_memory_value == MAP_FAILED) {
+        switch (errno) {
+            case 0:
+                break;
+            case EBADF:
+                printf("fd is not a valid open file descriptor");
+                return 1;
+            case EINVAL:
+                printf("The length argument was negative or larger than the maximum file size");
+                return 1;
+            case EFBIG:
+                printf("The length argument was larger than the current file size");
+                return 1;
+            case ENOMEM:
+                printf("There was insufficient room in the address space to map the file");
+                return 1;
+            case EACCES:
+                printf("fd does not have permission to map the file");
+                return 1;
+            case EAGAIN:
+                printf("The file has been locked, or too much memory has been locked");
+                return 1;
+            case ENODEV:
+                printf("The underlying filesystem of the specified file does not support memory mapping");
+                return 1;
+            default:
+                printf("Error mapping memory");
+                return 1;
+        }
+    }
+
     for (int i = 0; i < N_ELEMS; i++) {
         *shared_memory_value[i] = NUMBERS[i];
     }
     int counter = 0;
     int sorted = 0;
     while (!sorted) {
-        counter++;
-        if (counter % 10000000 == 0) {
-            int sorted_temp = 1;
-            for (int i = 0; i < N_ELEMS; i++) {
-                printf("%d", *shared_memory_value[i]);
-                if (i >= 1 && *shared_memory_value[i - 1] > *shared_memory_value[i]) {
-                    sorted_temp = 0;
-                }
+        int sorted_temp = 1;
+        for (int i = 0; i < N_ELEMS; i++) {
+            printf("%d", *shared_memory_value[i]);
+            if (i >= 1 && *shared_memory_value[i - 1] > *shared_memory_value[i]) {
+                sorted_temp = 0;
             }
-            sorted = sorted_temp;
-            printf("\n");
+        }
+        sorted = sorted_temp;
+        printf("\n");
+        sleep(1);
+    }
+    if (munmap(shared_memory_value, sizeof(shared_memory_value)) == -1) {
+        switch (errno) {
+            case 0:
+                break;
+            case EBADF:
+                printf("fd is not a valid open file descriptor");
+                return 1;
+            case EINVAL:
+                printf("The length argument was negative or larger than the maximum file size");
+                return 1;
+            case EFBIG:
+                printf("The length argument was larger than the current file size");
+                return 1;
+            case ENOMEM:
+                printf("There was insufficient room in the address space to map the file");
+                return 1;
+            case EACCES:
+                printf("fd does not have permission to map the file");
+                return 1;
+            case EAGAIN:
+                printf("The file has been locked, or too much memory has been locked");
+                return 1;
+            case ENODEV:
+                printf("The underlying filesystem of the specified file does not support memory mapping");
+                return 1;
+            default:
+                printf("Error unmapping memory");
+                return 1;
         }
     }
-    munmap(shared_memory_value, sizeof(shared_memory_value));
+    if (close(shared_memory_file_descriptor) == -1) {
+        switch (errno) {
+            case 0:
+                break;
+            case EBADF:
+                printf("fd is not a valid open file descriptor");
+                return 1;
+            case EINTR:
+                printf("close() was interrupted by a signal");
+                return 1;
+            case EIO:
+                printf("An I/O error occurred");
+                return 1;
+            default:
+                printf("Error closing file");
+                return 1;
+        }
+    }
     return 0;
 }
--- pi_thread.c
+++ pi_thread.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <pthread.h>
 #include <time.h>
+#include <errno.h>
 unsigned long num_iterations_per_thread = 0;
 unsigned long num_iterations = 0;
 unsigned long num_threads = 1;
@@ -15,11 +16,12 @@ typedef struct thread_data {
 
 void* calc_pi_helper(void* args) {
     thread_data* tdata = (thread_data*)args;
-    // srand(tdata->idx);
+    // initialen seed für jeden thread anders
     unsigned int seed = tdata->idx;
     unsigned long count = 0;
 
     for (unsigned long i = 0; i < num_iterations_per_thread; i++) {
+        // rand_r ist thread safe
         float x = (float)rand_r(&seed) / (float)(RAND_MAX);
         float y = (float)rand_r(&seed) / (float)(RAND_MAX);
         if (x * x + y * y <= 1.) {
@@ -32,7 +34,24 @@ void* calc_pi_helper(void* args) {
 
 int main(int argc, char** argv) {
     struct timespec start, end;
-    clock_gettime(CLOCK_MONOTONIC, &start);
+    if (clock_gettime(CLOCK_MONOTONIC, &start)) {
+        switch (errno) {
+            case 0:
+                break;
+            case EFAULT:
+                printf("tp points outside the accessible address space");
+                return 1;
+            case EINVAL:
+                printf("The clk_id specified is not supported on this system");
+                return 1;
+            case EPERM:
+                printf("clock_settime() does not have permission to set the clock indicated");
+                return 1;
+            default:
+                printf("Error getting time");
+                return 1;
+        }
+    }
 
     for (int i = 0; i < argc; i++) {
         if (strcmp(argv[i], "-n") == 0 && i < argc - 1) {
@@ -48,19 +67,70 @@ int main(int argc, char** argv) {
     for (unsigned long i = 0; i < num_threads; i++) {
         tdata[i].res = 0;
         tdata[i].idx = i;
-        pthread_create(&threads[i], NULL, &calc_pi_helper, &tdata[i]);
+        if (pthread_create(&threads[i], NULL, &calc_pi_helper, &tdata[i])) {
+            switch (errno) {
+                case 0:
+                    break;
+                case EAGAIN:
+                    printf("Insufficient resources to create another thread");
+                    return 1;
+                case EINVAL:
+                    printf("Invalid settings in attr");
+                    return 1;
+                case EPERM:
+                    printf("No permission to set the scheduling policy and parameters specified in attr");
+                    return 1;
+                default:
+                    printf("Error creating thread");
+                    return 1;
+            }
+        }
     }
+
     for (unsigned long i = 0; i < num_threads; i++) {
-        pthread_join(threads[i], NULL);
+        if (pthread_join(threads[i], NULL)) {
+            switch (errno) {
+                case 0:
+                    break;
+                case EDEADLK:
+                    printf("A deadlock was detected (e.g., two threads tried to join with each other); or thread specifies the calling thread");
+                    return 1;
+                case EINVAL:
+                    printf("thread is not a joinable thread");
+                    return 1;
+                case ESRCH:
+                    printf("No thread with the ID thread could be found");
+                    return 1;
+                default:
+                    printf("Error joining thread");
+                    return 1;
+            }
+        }
     }
     unsigned long sum = 0;
     for (unsigned long i = 0; i < num_threads; i++) {
         sum += tdata[i].res;
     }
-    clock_gettime(CLOCK_MONOTONIC, &end);
+    if (clock_gettime(CLOCK_MONOTONIC, &end)) {
+        switch (errno) {
+            case 0:
+                break;
+            case EFAULT:
+                printf("tp points outside the accessible address space");
+                return 1;
+            case EINVAL:
+                printf("The clk_id specified is not supported on this system");
+                return 1;
+            case EPERM:
+                printf("clock_settime() does not have permission to set the clock indicated");
+                return 1;
+            default:
+                printf("Error getting time");
+                return 1;
+        }
+    }
 
-    double delta = (end.tv_sec - start.tv_sec);
-    delta += (end.tv_nsec - start.tv_nsec) / 1000000000.0;
+    double delta = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
     printf("RESULT: %lf \nTIME: %f", 4 * (double)sum / (double)num_iterations, delta);
     return 0;
 }
